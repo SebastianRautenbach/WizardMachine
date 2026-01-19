1843f90c96c75f0521d71739ff55390b0785a64b
Sebastian Rautenbach <55651172+SebastianRautenbach@users.noreply.github.com>
2026-01-19 22:14:26 +0200

    Camera update

--- engine/WizmRender/includes/core/camera3D.h
+++ engine/WizmRender/includes/core/camera3D.h
@@ -9,81 +9,36 @@ using namespace wizmcore;
 
 namespace wizmrenderer {
 	class camera3D : public core_camera {
+	
 	public:
-
-		void set_model_view_matrix() override;
-		void set_projection_matrix() override;
-
-
-		camera3D (
-		float fov = 60.f,
-		float near_plane = 0.1f,
-		float far_plane = 1000.f,
-		float speed = 0.01f
-		);
-		
+		camera3D();
 		~camera3D() = default;
 		
-		void set_fov(float fov);
-		void set_aspect_ratio(float aspect_ratio);
-		
-		void set_far_plane(float far_plane);
-		void set_near_plane(float near_plane);
-		
-		void reset_rotation();
-		
-		//--------------------------------------------- TRANSFORMATION
-		
-		void set_position(const math::vec3& position);
-		void set_rotation(const math::vec3& rotation);
-		void set_rotation_matrix(const math::mat4& rotation_matrix);
-		
-		const math::vec3& get_position() const;
-		
-		void add_yaw(float yaw);
-		void add_pitch(float pitch);
-		void add_roll(float roll);
-		
-		void set_yaw(float yaw);
-		void set_pitch(float pitch);
-		void set_roll(float roll);
-		
-		void move_up(float distance);
-		void move_right(float distance);
-		void move_forward(float distance);
-		
-		void set_speed(float speed);
-		void add_speed(float speed);
-		
-		
-		const math::mat4& get_view_matrix();
-		const math::mat4& get_projection_matrix();
-		const math::mat4& get_rotation_matrix() const;
-		
-		math::vec3 get_forward_vector() const;
-		math::vec3 get_right_vector() const;
-		math::vec3 get_up_vector() const;
-		
 	public:
+		void set_model_view_matrix() override;
+		void set_projection_matrix() override;
 		e_camera_type get_camera_type() override;
-
-	private:
-		e_camera_type m_type;
+	
+	public:
+		math::vec3 get_forward();
+		math::vec3 get_right();
+		math::vec3 get_up();
 		
-		float m_fov;
-		float m_aspect_ratio;
+	private:
+	
+		float m_pitch;
+		float m_yaw;
+		float m_roll;
+		
 		float m_near_plane;
 		float m_far_plane;
-		float m_speed;
 		
 		math::vec3 m_position;
 		
-		math::mat4 m_projection_matrix;
-		math::mat4 m_view_matrix;
-		math::mat4 m_rotation_matrix = math::mat4(1.f);
+		math::mat4 m_view_matrix, m_prev_view_matrix;
+		math::mat4 m_projection_matrix, m_prev_projection_matrix;
 		
-		bool m_has_view_updated;
-		bool m_has_projection_updated;
-
+		float m_sensitivity = 0.001;
+		float mSpeed = 5.0f;
 	};
 }
